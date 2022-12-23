55577538f10c9be82271ac7f2cc05c7261a42648
unknown <ikushonimonena@gmail.com>
2022-12-22 20:51:54 -0600

    first commit

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    "pk_test_51MH1fgJltx9JWnHM1jyi6DTHuTwv86nW4B5mFPgsd6cbE0fofenpF1pgjF2lMn6LQPitAJBXx97fFCD4JNk9FbnO00JQ2gNVsU";
+
+  const onToken = (token) => {
+    console.log(token);
+    alert("Payment Successful");
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="CRWN Clothing Ltd."
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,49 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
+import "firebase/compat/auth";
+
+const config = {
+  apiKey: "AIzaSyDl6XFQG2NzceTxKgEU_buFD8ISA-jOQQg",
+  authDomain: "crwn-mi.firebaseapp.com",
+  projectId: "crwn-mi",
+  storageBucket: "crwn-mi.appspot.com",
+  messagingSenderId: "718063464178",
+  appId: "1:718063464178:web:aa10d53ee6941169c26a6b",
+  measurementId: "G-JBRQKBLJFZ",
+};
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData,
+      });
+    } catch (error) {
+      console.log("error creating user", error.message);
+    }
+  }
+
+  return userRef;
+};
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: "select_account" });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
